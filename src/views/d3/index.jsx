754839ff2d8fed3a7176c35d21107c4bf1db5187
liuxionghui <liuxionghui@ludashi.com>
2022-02-09 19:53:15 +0800

    update

--- src/views/d3/index.jsx
+++ src/views/d3/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import * as d3 from 'd3'
+import './index.less'
 
 const D3Page = () => {
+  const init = () => {
+    const width = 600, height = 400
+    const svg = d3.select('#d3box').append('svg')
+    svg.attr('width', width).attr('height', height).attr('viewbox', `0 0 ${width} ${height}`)
+    const margin = {l: 60, t: 60, r: 60, b: 60}
+
+
+    const dataset = [2.5, 1.1, 3.8, 0.4, 1.9]
+
+  }
+
+  
+  useEffect(() => {
+    init()
+  }, [])
   return (
-    <div className="d3page">
-      d3
+    <div className="dpage" style={{height: 600}}>
+      <div id="d3box">
+        
+      </div>
     </div>
   )
 }
